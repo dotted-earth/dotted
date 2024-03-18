3a9e634b48d421ee7b171aa6cab0324247ee6b29
Billy Le <hi@billyle.dev>
2024-03-18 16:37:13 +0800

    add basic itineraries ui

--- lib/pages/itineraries_page.dart
+++ lib/pages/itineraries_page.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
-class ItinerariesPage extends StatelessWidget {
+class ItinerariesPage extends StatefulWidget {
   const ItinerariesPage({super.key});
+
+  @override
+  State<ItinerariesPage> createState() => _ItinerariesPageState();
+}
+
+class _ItinerariesPageState extends State<ItinerariesPage>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Itineraries")),
-    );
+        appBar: AppBar(
+          title: const Text("Itineraries"),
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: const <Widget>[
+              Tab(
+                text: "Upcoming",
+              ),
+              Tab(
+                text: "Past",
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: const <Widget>[
+            Center(
+              child: Text("There's nothing here"),
+            ),
+            Center(
+              child: Text("There's nothing here"),
+            ),
+          ],
+        ));
   }
 }
