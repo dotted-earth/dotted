35fc0dc3753a736fc0f47b6b0eb0fdde5efbda58
Billy Le <hi@billyle.dev>
2024-03-18 14:59:48 +0800

    add basic pages for navigation

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:touchdown/constants/routes.dart';
 import 'package:touchdown/constants/supabase.dart';
 import 'package:collection/collection.dart';
