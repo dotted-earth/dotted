17d7fc0a0f7efd64091090ea09bc89944f62d487
Billy Le <hi@billyle.dev>
2024-03-19 12:04:34 +0800

    fix env naming

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -5,7 +5,7 @@ part 'env.g.dart';
 @Envied(path: '.env.dev', useConstantCase: true)
 abstract class Env {
   @EnviedField(varName: "SUPABASE_URL")
-  static const String supabaseURL = _Env.supabaseUrl;
+  static const String supabaseURL = _Env.supabaseURL;
 
   @EnviedField(varName: "SUPABASE_ANON")
   static const String supabaseAnon = _Env.supabaseAnon;
