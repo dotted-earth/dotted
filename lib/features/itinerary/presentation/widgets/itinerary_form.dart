e66a4564170d2997624f98a4abe0880ee78f2dc9
Billy Le <hi@billyle.dev>
2024-03-26 03:39:23 +0800

    add ItineraryForm state

--- lib/features/itinerary/presentation/widgets/itinerary_form.dart
+++ lib/features/itinerary/presentation/widgets/itinerary_form.dart
@@ -13,30 +13,93 @@ class ItineraryForm extends StatefulWidget {
 }
 
 class _ItineraryFormState extends State<ItineraryForm> {
-  var _selectedIndex = 0;
-  List<Widget> screens = [];
+  final TextEditingController _destinationController = TextEditingController();
+  final TextEditingController _lengthOfStayController = TextEditingController();
+  final TextEditingController _accommodationController =
+      TextEditingController();
+  final TextEditingController _budgetController = TextEditingController();
+  DateTime _startDate = DateTime.now();
+  DateTime _endDate = DateTime.now();
+  bool _needAirportTransportation = false;
+  int _selectedIndex = 0;
 
-  void onSelectedIndex(int index) {
+  @override
+  void initState() {
+    super.initState();
+
+    _lengthOfStayController.addListener(
+      () {
+        if (_lengthOfStayController.text.isNotEmpty) {
+          final lengthOfDays = int.tryParse(_lengthOfStayController.text) ?? 0;
+          setState(() {
+            _endDate = _startDate.subtract(Duration(days: lengthOfDays));
+          });
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _destinationController.dispose();
+    _lengthOfStayController.dispose();
+    _accommodationController.dispose();
+    _budgetController.dispose();
+
+    super.dispose();
+  }
+
+  void _onSelectedIndex(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
+  void _onNeedAirportTransportation(bool? value) {
     setState(() {
-      screens = [
-        DestinationForm(onSelectedIndex: onSelectedIndex),
-        AccommodationForm(
-          onSelectedIndex: onSelectedIndex,
-        ),
-      ];
+      _needAirportTransportation = value ?? false;
     });
+  }
+
+  void _onStartDateChange(dynamic date) {
+    if (date is DateTime) {
+      setState(() {
+        _startDate = date;
+        if (_lengthOfStayController.text.isNotEmpty) {
+          final lengthOfDays = int.tryParse(_lengthOfStayController.text) ?? 0;
+          _endDate = date.subtract(Duration(days: lengthOfDays));
+        }
+      });
+    }
+  }
+
+  void _onGenerateItinerary() {
+    // TODO - validate form
+    // TODO - create itinerary
+    // TODO - close bottom sheet if successful
+    // TODO - refetch upcoming itineraries
+    Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
+    final screens = [
+      DestinationForm(
+        onSelectedIndex: _onSelectedIndex,
+        destinationController: _destinationController,
+        lengthOfStayController: _lengthOfStayController,
+        onStartDateChange: _onStartDateChange,
+      ),
+      AccommodationForm(
+        onSelectedIndex: _onSelectedIndex,
+        accommodationController: _accommodationController,
+        budgetController: _budgetController,
+        needAirportTransportation: _needAirportTransportation,
+        onNeedAirportTransportation: _onNeedAirportTransportation,
+        onGenerateItinerary: _onGenerateItinerary,
+      )
+    ];
+
     return Form(
       child: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -47,18 +110,30 @@ class _ItineraryFormState extends State<ItineraryForm> {
 }
 
 class DestinationForm extends StatelessWidget {
-  const DestinationForm({super.key, required this.onSelectedIndex});
+  const DestinationForm({
+    super.key,
+    required this.onSelectedIndex,
+    required this.onStartDateChange,
+    required this.destinationController,
+    required this.lengthOfStayController,
+  });
   final ValueChanged<int> onSelectedIndex;
+  final ValueChanged<DateTime> onStartDateChange;
+  final TextEditingController destinationController;
+  final TextEditingController lengthOfStayController;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const TextField(
+        TextField(
           autofocus: true,
-          decoration: InputDecoration(
-              border: OutlineInputBorder(), labelText: "Destination"),
+          controller: destinationController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Destination",
+          ),
         ),
         formControl,
         ElevatedButton.icon(
@@ -79,8 +154,8 @@ class DestinationForm extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   fontSize: 16,
                 ),
-                onSubmit: (index) {
-                  print(index);
+                onSubmit: (date) {
+                  onStartDateChange(date);
                 },
                 bottomPickerTheme: BottomPickerTheme.plumPlate,
               ).show(context);
@@ -88,9 +163,10 @@ class DestinationForm extends StatelessWidget {
             icon: const Icon(Icons.calendar_month),
             label: const Text("Start Date")),
         formControl,
-        const TextField(
-          keyboardType: TextInputType.number,
-          decoration: InputDecoration(
+        TextField(
+          controller: lengthOfStayController,
+          keyboardType: const TextInputType.numberWithOptions(),
+          decoration: const InputDecoration(
             border: OutlineInputBorder(),
             labelText: "Length of Stay",
             constraints: BoxConstraints(maxWidth: 141),
@@ -124,28 +200,35 @@ enum ScheduleLength {
   wholeDay,
 }
 
-class AccommodationForm extends StatefulWidget {
-  const AccommodationForm({super.key, required this.onSelectedIndex});
+class AccommodationForm extends StatelessWidget {
+  const AccommodationForm({
+    super.key,
+    required this.onSelectedIndex,
+    required this.onNeedAirportTransportation,
+    required this.needAirportTransportation,
+    required this.budgetController,
+    required this.accommodationController,
+    required this.onGenerateItinerary,
+  });
   final ValueChanged<int> onSelectedIndex;
-
-  @override
-  State<AccommodationForm> createState() => _AccommodationFormState();
-}
-
-class _AccommodationFormState extends State<AccommodationForm> {
-  Airport? _airport = Airport.no;
-  TravelIntensity? _travelIntensity = TravelIntensity.moderate;
+  final ValueChanged<bool?> onNeedAirportTransportation;
+  final bool needAirportTransportation;
+  final TextEditingController budgetController;
+  final TextEditingController accommodationController;
+  final VoidCallback onGenerateItinerary;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const TextField(
+        TextField(
           autofocus: true,
-          decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Hotel you're staying with?"),
+          controller: accommodationController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Place you're staying",
+          ),
         ),
         formControl,
         const Text("Need transport from airport?"),
@@ -157,14 +240,10 @@ class _AccommodationFormState extends State<AccommodationForm> {
                 contentPadding: const EdgeInsets.all(0),
                 horizontalTitleGap: 0,
                 title: const Text('No'),
-                leading: Radio<Airport>(
-                  value: Airport.no,
-                  groupValue: _airport,
-                  onChanged: (Airport? value) {
-                    setState(() {
-                      _airport = value;
-                    });
-                  },
+                leading: Radio<bool>(
+                  value: false,
+                  groupValue: needAirportTransportation,
+                  onChanged: onNeedAirportTransportation,
                 ),
               ),
             ),
@@ -174,72 +253,22 @@ class _AccommodationFormState extends State<AccommodationForm> {
                 contentPadding: const EdgeInsets.all(0),
                 horizontalTitleGap: 0,
                 title: const Text('Yes'),
-                leading: Radio<Airport>(
-                  value: Airport.yes,
-                  groupValue: _airport,
-                  onChanged: (Airport? value) {
-                    setState(() {
-                      _airport = value;
-                    });
-                  },
+                leading: Radio<bool>(
+                  value: true,
+                  groupValue: needAirportTransportation,
+                  onChanged: onNeedAirportTransportation,
                 ),
               ),
             ),
           ],
         ),
-        const Text("Intensity"),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: ListTile(
-                contentPadding: const EdgeInsets.all(0),
-                horizontalTitleGap: 0,
-                title: const Text('Casual'),
-                leading: Radio<TravelIntensity>(
-                  value: TravelIntensity.casual,
-                  groupValue: _travelIntensity,
-                  onChanged: (TravelIntensity? value) {
-                    setState(() {
-                      _travelIntensity = value;
-                    });
-                  },
-                ),
-              ),
-            ),
-            Expanded(
-              child: ListTile(
-                contentPadding: const EdgeInsets.all(0),
-                horizontalTitleGap: 0,
-                title: const Text('Moderate'),
-                leading: Radio<TravelIntensity>(
-                  value: TravelIntensity.moderate,
-                  groupValue: _travelIntensity,
-                  onChanged: (TravelIntensity? value) {
-                    setState(() {
-                      _travelIntensity = value;
-                    });
-                  },
-                ),
-              ),
-            ),
-            Expanded(
-              child: ListTile(
-                contentPadding: const EdgeInsets.all(0),
-                horizontalTitleGap: 0,
-                title: const Text('Sprint'),
-                leading: Radio<TravelIntensity>(
-                  value: TravelIntensity.sprint,
-                  groupValue: _travelIntensity,
-                  onChanged: (TravelIntensity? value) {
-                    setState(() {
-                      _travelIntensity = value;
-                    });
-                  },
-                ),
-              ),
-            ),
-          ],
+        TextField(
+          controller: budgetController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Budget",
+          ),
+          keyboardType: const TextInputType.numberWithOptions(),
         ),
         formControl,
         Row(
@@ -247,12 +276,12 @@ class _AccommodationFormState extends State<AccommodationForm> {
           children: [
             IconButton(
               onPressed: () {
-                widget.onSelectedIndex(0);
+                onSelectedIndex(0);
               },
               icon: const Icon(Icons.arrow_back),
             ),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: onGenerateItinerary,
               icon: const Icon(Icons.auto_awesome),
               label: const Text("Generate"),
             ),
