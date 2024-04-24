3c51a4c6178169e49620cb2a3f9b1f4b14e3385a
Billy Le <hi@billyle.dev>
2024-04-24 19:00:17 +0800

    add sign in with Apple

--- lib/features/auth/bloc/auth_event.dart
+++ lib/features/auth/bloc/auth_event.dart
@@ -9,6 +9,10 @@ final class AuthLoginWithGoogleRequested extends AuthEvent {
   const AuthLoginWithGoogleRequested();
 }
 
+final class AuthLoginWithAppleRequested extends AuthEvent {
+  const AuthLoginWithAppleRequested();
+}
+
 final class AuthLogOutRequested extends AuthEvent {
   const AuthLogOutRequested();
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import app_links
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
+import sign_in_with_apple
 import sqlite3_flutter_libs
 import url_launcher_macos
 
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
